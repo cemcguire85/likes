f2a4b4e6eee09cd20daa112b135c726707e23a56
Chuck McGuire <cemcguire85@gmail.com>
2022-03-04 23:55:22 -0500

    initial commit

---
+++ part_one/script.js
@@ -0,0 +1,7 @@
+var count = 0;
+var numberElement = document.querySelector("#number");
+
+function add1() {
+    count++;
+    numberElement.innerText = count;
+}
---
+++ part_two/script.js
@@ -0,0 +1,23 @@
+var countOne = 0;
+var neilElement = document.querySelector("#numberone");
+
+function add1Neil() {
+    countOne++;
+    neilElement.innerText = countOne;
+}
+
+var countTwo = 0;
+var nicholeElement = document.querySelector("#numbertwo");
+
+function add1Nichole() {
+    countTwo++;
+    nicholeElement.innerText = countTwo;
+}
+
+var countThree = 0;
+var jimElement = document.querySelector("#numberthree");
+
+function add1Jim() {
+    countThree++;
+    jimElement.innerText = countThree;
+}
